2a69c40a41fafbbf1969d48d67853be19e6494b9
Rushikesh Jogdand <rushikeshjogdand1@gmail.com>
2019-04-03 22:10:27 +0530

    people:ux: initial implementation
    - People list is populated with fake data.
    - Clicking on person's name takes to personDataFragment.

--- app/src/main/java/com/example/wipi/data_display/PeopleListFragment.java
+++ app/src/main/java/com/example/wipi/data_display/PeopleListFragment.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.wipi.R;
+import com.example.wipi.models.Person;
+
+import java.util.List;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 /**
@@ -16,6 +20,7 @@ import androidx.fragment.app.Fragment;
  */
 public class PeopleListFragment extends Fragment {
 
+    private List<Person> people = Fake.getPeopleList();
 
     public PeopleListFragment() {
     }
@@ -25,6 +30,8 @@ public class PeopleListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_people_list, container, false);
+        final RecyclerView peopleListRv = v.findViewById(R.id.rv_people_list);
+        peopleListRv.setAdapter(new PersonAdapter(people));
         return v;
     }
 
---
+++ app/src/main/java/com/example/wipi/data_display/PersonVh.java
@@ -0,0 +1,32 @@
+package com.example.wipi.data_display;
+
+import android.view.View;
+
+import com.example.wipi.R;
+import com.example.wipi.models.Person;
+import com.google.android.material.button.MaterialButton;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * @author Rushikesh Jogdand.
+ */
+public class PersonVh extends RecyclerView.ViewHolder {
+
+    private MaterialButton nameBtn;
+
+    public PersonVh(@NonNull final View itemView) {
+        super(itemView);
+        nameBtn = itemView.findViewById(R.id.btn_person_name_people_list);
+    }
+
+    public void populate(Person person) {
+        nameBtn.setText(person.name);
+        itemView.setOnClickListener(v -> {
+            PeopleListFragmentDirections.ShowPersonDataAction action = PeopleListFragmentDirections.showPersonDataAction(person.deviceMac);
+            Navigation.findNavController(v).navigate(action);
+        });
+    }
+}
